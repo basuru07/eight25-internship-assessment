7eb20e7c93eba7f917e46ed55c3bda6ded3b9919
basuru07 <yasaruwanbasuru637@gmail.com>
2025-04-19 16:21:44 +0530

    feat: done first 3 pages

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const discoverBtn = document.querySelector('.discover-btn');
+    discoverBtn.addEventListener('click', () => {
+        alert('Discovering success stories...');
+    });
+});
